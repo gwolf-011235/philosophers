249e0ff84228a408873880eddd04ca9e27691a83
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-06-09 21:53:39 +0200

    fix(input): refactor err msg

--- philo/input.c
+++ philo/input.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 18:08:56 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/09 11:51:55 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/09 21:52:47 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_err	ft_check_args(int argc, char **argv)
 
 	if (argc < 5 || argc > 6)
 	{
-		printf("Wrong number of arguments. Expected 4 [5], got: %d\n", argc - 1);
+		printf("⚠️  Expected 4 [5] args, got: %d\n", argc - 1);
 		return (ERR_NUMARGS);
 	}
 	while (argc-- > 1)
@@ -28,20 +28,13 @@ t_err	ft_check_args(int argc, char **argv)
 		{
 			if (ft_isdigit(argv[argc][i]) != SUCCESS)
 			{
-				printf("Only digits are allowed: %s (%d)\n", argv[argc], argc);
-				return (ERR_INPUT);
+				printf("⚠️  Not digit: %s (%d)\n", argv[argc], argc);
+				return (ERR_NOTDIGIT);
 			}
 			i++;
 		}
 	}
 	return (0);
-}
-
-t_err	ft_print_usage(char *prog_name, t_err err)
-{
-	printf("\nusage: %s number_of_philosophers time_to_die time_to_sleep \
-[number_of_times_each_philosopher_must_eat]\n", prog_name);
-	return (err);
 }
 
 t_err	ft_set_params(t_params *params, int argc, char **argv)
@@ -55,9 +48,9 @@ t_err	ft_set_params(t_params *params, int argc, char **argv)
 	if (params->num_philos < 0 || params->time_to_die < 0
 		|| params->time_to_eat < 0 || params->time_to_sleep < 0
 		|| (argc == 6 && params->times_to_eat < 0))
-	{
-		printf("Args need to be <= UINT_MAX\n");
 		return (ERR_OVERFLOW);
-	}
+	params->start_time = ft_timestamp_in_ms();
+	if (params->start_time == -1)
+		return (ERR_TIME);
 	return (SUCCESS);
 }
--- philo/input.h
+++ philo/input.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 11:20:55 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/09 11:42:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/09 21:12:17 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 # include "philo_struct.h"
 # include "return_codes.h"
 # include "utils.h"
+# include "time.h"
 
 t_err	ft_check_args(int argc, char **argv);
-t_err	ft_print_usage(char *prog_name, t_err err);
 t_err	ft_set_params(t_params *params, int argc, char **argv);
 
 #endif
