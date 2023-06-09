545b90d30ff2c69927364e96f55cef1cf83cb924
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-06-09 21:50:22 +0200

    fix(time): add error return change back to int64_t

--- philo/time.c
+++ philo/time.c
@@ -6,18 +6,19 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 17:29:38 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/09 11:23:41 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/09 21:23:04 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "time.h"
 
-uint64_t	ft_timestamp_in_ms(void)
+int64_t	ft_timestamp_in_ms(void)
 {
 	struct timeval	tv;
-	uint64_t		ms;
+	int64_t			ms;
 
-	gettimeofday(&tv, NULL);
+	if (gettimeofday(&tv, NULL) == -1)
+		return (-1);
 	ms = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
 	return (ms);
 }
--- philo/time.h
+++ philo/time.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 11:19:37 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/09 11:24:40 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/09 20:41:29 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 # include <stdint.h>
 # include <stddef.h>
 
-uint64_t	ft_timestamp_in_ms(void);
+int64_t	ft_timestamp_in_ms(void);
 
 #endif
