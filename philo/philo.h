0a535599771fb29d5d5ecba88d516bc64f5d26e5
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-06-09 12:00:14 +0200

    feat: combine headers

--- philo/philo.h
+++ philo/philo.h
@@ -6,27 +6,17 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 15:33:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/08 18:35:07 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/09 11:23:19 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <stdint.h>
-# include <unistd.h>
-# include <sys/time.h>
-
-# define ERROR 1
-
-//input.c
-int		ft_check_args(int argc, char **argv);
-int		ft_print_usage(char *prog_name);
-
-//time.c
-int64_t	ft_timestamp_in_ms(void);
+# include "input.h"
+# include "philo_struct.h"
+# include "return_codes.h"
+# include "time.h"
+# include "utils.h"
 
 #endif
