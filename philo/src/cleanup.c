e7dbfcb0f65da70eaf21f7be010b6fb37cbd7291
Gabriel Wolf <gwolf@student.42vienna.com>
2023-06-13 08:56:09 +0200

    fix(cleanup): issue t_err would always be success

--- philo/src/cleanup.c
+++ philo/src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 00:29:45 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/13 00:31:39 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/13 08:54:19 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,14 @@ t_err	ft_cleanup(t_data *data, t_err err, int32_t num_philos)
 {
 	if (data->stage == POST_MUTEX_PHILO)
 	{
-		err = ft_m_destroy_philo_status(data->philos, num_philos);
-		if (err != SUCCESS)
+		if (ft_m_destroy_philo_status(data->philos, num_philos) != SUCCESS)
 			return (ft_print_err_msg(PANIC));
-		err = ft_m_destroy_philo_last_meal(data->philos, num_philos);
-		if (err != SUCCESS)
+		if (ft_m_destroy_philo_last_meal(data->philos, num_philos) != SUCCESS)
 			return (ft_print_err_msg(PANIC));
 	}
 	if (data->stage >= POST_MUTEX_FORK)
 	{
-		err = ft_m_destroy_forks_in_use(data->forks, num_philos);
-		if (err != SUCCESS)
+		if (ft_m_destroy_forks_in_use(data->forks, num_philos) != SUCCESS)
 			return (ft_print_err_msg(PANIC));
 	}
 	if (data->stage >= POST_MUTEX_DATA)
