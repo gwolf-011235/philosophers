4586f9cde6b6cfd20d173cd1884ea3a10e12c775
Gabriel <gwolf@student.42vienna.com>
2023-06-27 09:15:13 +0200

    refactor: philo.h got smaller

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,19 +6,19 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 15:33:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/16 14:34:36 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/27 09:10:01 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include "input.h"
 # include "philo_struct.h"
 # include "return_codes.h"
-# include "utils.h"
-# include "init.h"
-# include "routine.h"
-# include "cleanup.h"
+
+t_err	ft_handle_input(t_params *params, int argc, char **argv, bool *meals);
+t_err	ft_init_sim(t_data *data, t_params *params);
+t_err	ft_start_sim(t_philo *philos, t_params *params, bool *meals);
+t_err	ft_cleanup(t_data *data, t_err err, int32_t num_philos);
 
 #endif
